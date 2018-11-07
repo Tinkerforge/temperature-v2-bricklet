9f8e3259f9df625078963cebfcf2c5b52225af8d
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:28:26 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,21 +1,21 @@
 use std::{error::Error, io};
 
-use tinkerforge::{ipconnection::IpConnection, temperature_v2_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, temperature_v2_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Temperature Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Temperature Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let temperature_v2_bricklet = TemperatureV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let t = TemperatureV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current temperature
-    let temperature = temperature_v2_bricklet.get_temperature().recv()?;
-    println!("Temperature: {}{}", temperature as f32 / 100.0, " °C");
+    // Get current temperature.
+    let temperature = t.get_temperature().recv()?;
+    println!("Temperature: {} °C", temperature as f32 / 100.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
