b737eb01a8945051a2d95df36aa2783835beed8f
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:41:40 +0100

    rust: Reformat examples.

--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{ip_connection::IpConnection, temperature_v2_bricklet::*};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  temperature_v2_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,23 +13,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let t = TemperatureV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    let temperature_receiver = t.get_temperature_callback_receiver();
+     let temperature_receiver = t.get_temperature_callback_receiver();
 
-    // Spawn thread to handle received callback messages.
-    // This thread ends when the `t` object
-    // is dropped, so there is no need for manual cleanup.
-    thread::spawn(move || {
-        for temperature in temperature_receiver {
-            println!("Temperature: {} °C", temperature as f32 / 100.0);
-            println!("It is too hot, we need air conditioning!");
-        }
-    });
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `t` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for temperature in temperature_receiver {           
+                		println!("Temperature: {} °C", temperature as f32 /100.0);
+		println!("It is too hot, we need air conditioning!");
+            }
+        });
 
-    // Configure threshold for temperature "greater than 30 °C"
-    // with a debounce period of 1s (1000ms).
-    t.set_temperature_callback_configuration(1000, false, '>', 30 * 100, 0);
+		// Configure threshold for temperature "greater than 30 °C"
+		// with a debounce period of 1s (1000ms).
+		t.set_temperature_callback_configuration(1000, false, '>', 30*100, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
