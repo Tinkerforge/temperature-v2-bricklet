f5e89580aceba9dcfbb06bc131290ba671983192
Erik Fleckstein <erik@tinkerforge.com>
2020-08-19 16:53:58 +0200

    Add uc examples.

---
+++ software/examples/uc/example_callback.c
@@ -0,0 +1,40 @@
+
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_temperature_v2.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your Temperature Bricklet 2.0
+
+void check(int rc, const char* msg);
+
+
+
+// Callback function for temperature callback
+void temperature_handler(TF_TemperatureV2 *device, int16_t temperature, void *user_data) {
+	(void)device; (void)user_data; // avoid unused parameter warning
+
+	tf_hal_printf("Temperature: %d 1/%d °C\n", temperature, 100.0);
+}
+
+
+TF_TemperatureV2 t;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_temperature_v2_create(&t, UID, hal), "create device object");
+
+
+	// Register temperature callback to function temperature_handler
+	tf_temperature_v2_register_temperature_callback(&t,
+	                                               temperature_handler,
+	                                               NULL);
+
+	// Set period for temperature callback to 1s (1000ms) without a threshold
+	tf_temperature_v2_set_temperature_callback_configuration(&t, 1000, false, 'x', 0, 0);
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
---
+++ software/examples/uc/example_threshold.c
@@ -0,0 +1,42 @@
+
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_temperature_v2.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your Temperature Bricklet 2.0
+
+void check(int rc, const char* msg);
+
+
+
+// Callback function for temperature callback
+void temperature_handler(TF_TemperatureV2 *device, int16_t temperature, void *user_data) {
+	(void)device; (void)user_data; // avoid unused parameter warning
+
+	tf_hal_printf("Temperature: %d 1/%d °C\n", temperature, 100.0);
+	tf_hal_printf("It is too hot, we need air conditioning!\n");
+}
+
+
+TF_TemperatureV2 t;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_temperature_v2_create(&t, UID, hal), "create device object");
+
+
+	// Register temperature callback to function temperature_handler
+	tf_temperature_v2_register_temperature_callback(&t,
+	                                               temperature_handler,
+	                                               NULL);
+
+	// Configure threshold for temperature "greater than 30 °C"
+	// with a debounce period of 1s (1000ms)
+	tf_temperature_v2_set_temperature_callback_configuration(&t, 1000, false, '>', 30*100, 0);
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
